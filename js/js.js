535523f1e7db4d5dcab7c80bdde8533d068aa387
tbrodtrick <59714453+tbrodtrick@users.noreply.github.com>
2024-04-02 02:11:12 -0700

    final project
    uploading finished project

---
+++ js/js.js
@@ -0,0 +1,85 @@
+//Tristan Brodtrick - 567169834
+
+//functions for hiding and showing subcontent in pages
+function thoughts(){
+    document.querySelectorAll(".content_header1").forEach(a=>a.style.display = "none");
+    document.getElementById("thoughts").style.display="inline-block";
+    document.querySelectorAll(".content_container1").forEach(a=>a.style.display = "none");
+    document.getElementById("thoughts_content").style.display="block";
+}
+
+function toppings(){
+
+    document.querySelectorAll(".content_header1").forEach(a=>a.style.display = "none");
+    document.getElementById("toppings").style.display="inline-block";
+    document.querySelectorAll(".content_container1").forEach(a=>a.style.display = "none");
+    document.getElementById("toppings_content").style.display="block";
+}
+
+function clearsoup(){
+
+    document.querySelectorAll(".content_header1").forEach(a=>a.style.display = "none");
+    document.getElementById("clearsoup").style.display="inline-block";
+    document.querySelectorAll(".content_container1").forEach(a=>a.style.display = "none");
+    document.getElementById("clearsoup_content").style.display="block";
+
+}
+function cloudysoup(){
+
+    document.querySelectorAll(".content_header1").forEach(a=>a.style.display = "none");
+    document.getElementById("cloudysoup").style.display="inline-block";
+    document.querySelectorAll(".content_container1").forEach(a=>a.style.display = "none");
+    document.getElementById("cloudysoup_content").style.display="block";
+
+}
+
+function intro(){
+    document.querySelectorAll(".content_header1").forEach(a=>a.style.display = "none");
+    document.getElementById("intro").style.display="inline-block";
+    document.querySelectorAll(".content_container1").forEach(a=>a.style.display = "none");
+    document.getElementById("intro_content").style.display="block";
+}
+
+function broth(){
+    document.querySelectorAll(".content_header1").forEach(a=>a.style.display = "none");
+    document.getElementById("broth").style.display="inline-block";
+    document.querySelectorAll(".content_container1").forEach(a=>a.style.display = "none");
+    document.getElementById("broth_content").style.display="block";
+}
+
+function seasoning(){
+    document.querySelectorAll(".content_header1").forEach(a=>a.style.display = "none");
+    document.getElementById("seasoning").style.display="inline-block";
+    document.querySelectorAll(".content_container1").forEach(a=>a.style.display = "none");
+    document.getElementById("seasoning_content").style.display="block";
+}
+
+function noodles(){
+    document.querySelectorAll(".content_header1").forEach(a=>a.style.display = "none");
+    document.getElementById("noodles").style.display="inline-block";
+    document.querySelectorAll(".content_container1").forEach(a=>a.style.display = "none");
+    document.getElementById("noodles_content").style.display="block";
+}
+
+function toppings1(){
+    document.querySelectorAll(".content_header1").forEach(a=>a.style.display = "none");
+    document.getElementById("toppings1").style.display="inline-block";
+    document.querySelectorAll(".content_container1").forEach(a=>a.style.display = "none");
+    document.getElementById("toppings1_content").style.display="block";
+
+}function fat(){
+    document.querySelectorAll(".content_header1").forEach(a=>a.style.display = "none");
+    document.getElementById("fat").style.display="inline-block";
+    document.querySelectorAll(".content_container1").forEach(a=>a.style.display = "none");
+    document.getElementById("fat_content").style.display="block";
+}
+
+
+
+//<div class="content_header1" id="intro">An Introduction</div>
+//<div class="content_header1" id="broth">Broth</div>
+//<div class="content_header1" id="seasoning">Seasoning</div>
+//<div class="content_header1" id="noodles">Noodles</div>
+//<div class="content_header1" id="toppings1">Toppings</div>
+//<div class="content_header1" id="fat">Fat</div>
+//<div class="content_container1" id="intro_content"></div>
